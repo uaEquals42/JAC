15a853a7c546b0aed21a484f7c31a9b27007ce0e
Gregory Jordan <grjordan@gmail.com>
2014-10-25 21:40:00 -0500

    Base stub.

---
+++ src/engine/Base.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package engine;
+
+/**
+ *
+ * @author grjordan
+ */
+public class Base {
+    int population;
+    int growth;
+    String name;
+    
+    
+    void Base(String name){
+        this.name = name;
+    }
+    
+}
