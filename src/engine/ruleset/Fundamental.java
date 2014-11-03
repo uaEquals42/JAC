fbc20beacbf393aeb37aa5536589616a6c718e9e
Gregory Jordan <grjordan@gmail.com>
2014-11-03 16:00:01 -0600

    Working on the first section data.  Figuring out what variables to use/discard.

--- src/engine/ruleset/Fundamental.java
+++ src/engine/ruleset/Fundamental.java
@@ -11,10 +11,11 @@ package engine.ruleset;
  * @author grjordan
  */
 public class Fundamental {
+
     int road_movement_rate;
     int nutrient_intake_per_citizen; //Nutrient intake requirement for citizens
     // TODO:  3,2      ; Numerator & Denominator for artillery fire damage
-    int max_artillery_range; 
+    int max_artillery_range;
     int max_airdrop_range; // without orbital insertion.
     int nutrient_cost_multiplier; // ??????
     int mineral_cost_multiplier; // ??????
@@ -22,63 +23,77 @@ public class Fundamental {
     int min_increase_limit;  // Limits mineral increase for mine without road in square
     int mine_nutrient_effect; //(0 or -1)
     int min_base_size_specialists; // 5,       ; Minimum base size to support specialists
-    /*
-1,       ; Drones induced by Genejack factory
-7,       ; Population limit w/o hab complex
-14,      ; Population limit w/o hab dome
-50,      ; Extra percentage cost of prototype LAND unit
-50,      ; Extra percentage cost of prototype SEA unit
-50,      ; Extra percentage cost of prototype AIR unit
-3,2,     ; Psi combat offense-to-defense ratio (LAND unit defending)
-1,1,     ; Psi combat offense-to-defense ratio (SEA unit defending)
-1,1,     ; Psi combat offense-to-defense ratio (AIR unit defending)
-10,      ; Players' starting energy reserves
-25,      ; Combat % -> intrinsic base defense
-0,       ; Combat % -> attacking along road
-0,       ; Combat % -> for attacking from higher elevation
-0,       ; Combat penalty % -> attacking from lower elevation
-25,      ; Combat % -> Mobile unit in open ground
-0,       ; Combat % -> Defend vs. mobile in rough
-25,      ; Combat % -> Infantry vs. Base
-50,      ; Combat penalty % -> attack after airdrop
-25,      ; Combat % -> Fanatic attack bonus
-50,      ; Combat % -> Land based guns vs. ship artillery bonus
-25,      ; Combat % -> Artillery bonus per level of altitude
-50,      ; Combat % -> Trance bonus defending vs. psi
-50,      ; Combat % -> Empath Song bonus attacking vs. psi
-50,      ; Combat penalty % -> Air superiority unit vs. ground unit
-100,     ; Combat % -> Air superiority unit vs. air unit
-50,      ; Combat penalty % -> Non-combat unit defending vs. combat unit
-50,      ; Combat % -> Comm Jammer unit defending vs. mobile unit
-100,     ; Combat % -> Bonus vs. ships caught in port
-100,     ; Combat % -> AAA bonus vs. air units
-25,      ; Combat % -> Defend in range of friendly Sensor
-10,      ; Combat % -> Psi attack bonus/penalty per +PLANET
-50,      ; Retool percent penalty for production change
-2,       ; Retool strictness (0 = Always Free, 1 = Free in Category, 2 = Free if Project, 3 = Never Free)
-10,      ; Retool exemption (first X minerals not affected by penalty)
-20,      ; Minimum # of turns between councils
-5,       ; Minerals for harvesting forest
-8,       ; Territory: max distance from base
-20,      ; Turns to corner Global Energy Market
-CentPsi, ; Technology to improve fungus squares
-CentPsi, ; Technology to ease fungus movement
-CentEmp, ; Technology to build roads in fungus
-Neural,  ; Technology to allow 2 special abilities for a unit
-Gene,    ; Technology to allow 3 nutrients in a square
-EcoEng,  ; Technology to allow 3 minerals in a square
-EnvEcon, ; Technology to allow 3 energy in a square
-Gravity, ; Technology to allow orbital insertion w/o Space Elevator
-EcoEng2, ; Technology for +1 mining platform bonus
-PlaEcon, ; Technology for economic victory
-1,       ; If non-zero, probe teams can steal technologies
-50,      ; Maximum % damage inflicted by arty versus units in base/bunker
-99,      ; Maximum % damage inflicted by arty versus units in open
-100,     ; Maximum % damage inflicted by arty versus units at sea
-1, 1     ; Numerator/Denominator for frequency of global warming (1,2 would be "half" normal warming).
-2100     ; Normal starting year
-2600     ; Normal ending year for lowest 3 difficulty levels
-2500     ; Normal ending year for highest 3 difficulty levels
-1        ; If non-zero, obliterating a base counts as an atrocity
-    */
+
+// 1,       ; Drones induced by Genejack factory  This should be loaded into the facility data.
+//7,       ; Population limit w/o hab complex   This should be loaded into the facility data.
+//14,      ; Population limit w/o hab dome      This should be loaded into the facility data.
+    int land_prototype; // Extra percentage cost of prototype land unit.
+    int sea_prototype;  // "         "        "  "     "      sea
+    int air_prototype;  // "         "        "  "     "      air
+
+    int[] psi_ratio_attack_land; // array of size 2.  Land unit defending.
+    int[] psi_ratio_attack_sea;  // sea unit defending.
+    int[] psi_ratio_attack_air; // air unit defending.
+
+    int default_starting_energy; //Players' starting energy reserves.  Should be a game start option as well.
+    int intrinsic_base_defence;  //Bonus for defenders in a friendly base.
+    int road_attack_bonus;  // bonus for attacking along a road.
+    int higher_elevation_bonus; // % bonus for attacking from high ground.
+    int lower_elevation_penalty; // % penalty when defending from lower ground.
+
+        //TODO:  I'm wondering if I can abstract this bonus stuff out so that adding unit types is easier.
+        /*
+     25,      ; Combat % -> Mobile unit in open ground
+     0,       ; Combat % -> Defend vs. mobile in rough
+     25,      ; Combat % -> Infantry vs. Base
+     50,      ; Combat penalty % -> attack after airdrop
+     25,      ; Combat % -> Fanatic attack bonus
+     50,      ; Combat % -> Land based guns vs. ship artillery bonus
+     25,      ; Combat % -> Artillery bonus per level of altitude
+     50,      ; Combat % -> Trance bonus defending vs. psi
+     50,      ; Combat % -> Empath Song bonus attacking vs. psi
+     50,      ; Combat penalty % -> Air superiority unit vs. ground unit
+     100,     ; Combat % -> Air superiority unit vs. air unit
+     50,      ; Combat penalty % -> Non-combat unit defending vs. combat unit
+     50,      ; Combat % -> Comm Jammer unit defending vs. mobile unit
+     100,     ; Combat % -> Bonus vs. ships caught in port
+     100,     ; Combat % -> AAA bonus vs. air units
+     25,      ; Combat % -> Defend in range of friendly Sensor
+     10,      ; Combat % -> Psi attack bonus/penalty per +PLANET
+     */
+    int retool_percent_cost; // % of minerals lost when retooling for a production change.
+    int retool_stricness; // (0 = Always Free, 1 = Free in Category, 2 = Free if Project, 3 = Never Free) TODO: Probably should change the values to an enum.
+    int retool_exception;  // first x minerals not affected by retooling penalty.
+
+    int turns_between_councils;  // Minimum # of turns between councils.
+    int forest_minerals; // Minerals gathered when you harvest a forest.
+    int territory_max;  // Territory distanse from a base.
+    int global_energy_turns; // # of turns to corner the   int global_energy_turns; // # of turns to cornerglobal energy market victory.
+
+    // TODO:  These vars should probably be something else for flexibility.
+    String tech_fungus_improve; //Build improvments in fungus squares.
+    String tech_fungus_move;    //Move at full speed in fungus.
+    String tech_fungus_roads;  // build roads in fungus
+
+    String tech_allow_2_abilities; //TODO: I KNOW this should be stored in the tech object.  and it should be for x abilities.
+    String tech_allow_3_nutrients; //TODO: I KNOW this should be stored in the tech object.
+    String tech_allow_3_minerals;//TODO: I KNOW this should be stored in the tech object.
+    String tech_allow_3_energy;//TODO: I KNOW this should be stored in the tech object.
+    String tech_allow_orbitial_insertion;//TODO: I KNOW this should be stored in the tech object.
+    String tech_allow_mining_platform_bonus;//TODO: I KNOW this should be stored in the tech object.
+    String tech_allow_economic_vicotry;//TODO: I KNOW this should be stored in the tech object.
+
+    boolean probe_teams_steal_tech;
+
+    int max_arty_damage_vs_base;
+    int max_arty_damage_vs_land;
+    int max_arty_damage_vs_sea;
+
+    int global_warming_level; // This will be a modifier TBD for determining when global warming will take place.
+
+    int starting_year;
+    int late_ending_year;
+    int early_ending_year;
+    boolean base_razing_atrocity;  // Is razing a base an atrocity?
+
 }
