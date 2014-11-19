e7608d6ac959d6dd527d654f6d2663931a0b8746
Gregory Jordan <grjordan@gmail.com>
2014-11-19 09:07:47 -0600

    add race restriction.

--- src/jac/unit/Restriction.java
+++ src/jac/unit/Restriction.java
@@ -1,8 +1,8 @@
 /*
  * JAC Copyright (C) 2014 Gregory Jordan
  *
- * This file is part of JAC.   
- * 
+ * This file is part of JAC.
+ *
  * JAC is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -19,13 +19,9 @@
 package jac.unit;
 
 import jac.Enum.CombatMode;
-import jac.engine.ruleset.Ideology;
 import jac.Enum.MovementType;
 import jac.engine.PlayerDetails;
-import java.util.ArrayList;
 import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 /**
@@ -33,86 +29,24 @@ import java.util.Set;
  * @author Gregory Jordan
  */
 public class Restriction {
-    Set<String> allowed_chassis;
-    Set<MovementType> allowedTypes;
-    Set<CombatMode> allowedRoles;
-    
-    Integer length_of_effect;  
-            
-    String required_ideology;
-    
-    Integer base_bigger_than;
-    Integer base_smaller_than;
-    String required_facility;
-    
-    
-   
-    
-    public static class Builder {
-        // Required
 
-        // Optional
-        private Set<String> allowed_chassis=new LinkedHashSet<>();
-        private Set<MovementType> allowedTypes=new LinkedHashSet<>();
-        private Set<CombatMode> allowedRoles=new LinkedHashSet<>();
-        
-        private Integer x_turns=null;
-      
-        private Integer base_bigger_than=null;
-        private Integer base_smaller_than=null;
-        
-        private String required_ideology="";
-        private String required_facility="";
-        
-        public Builder SetAllowedRoles(Set<CombatMode> commode){
-            this.allowedRoles = commode;
-            return this;
-        }
-        
-        public Builder SetAllowedTypes(Set<MovementType> allowedTypes){
-            this.allowedTypes = allowedTypes;
-            return this;
-        }
-        
-        public Builder addAllowedType(MovementType allowedType){
-            this.allowedTypes.add(allowedType);
-            return this;
-        }
-        
-        public Builder SetAllowedChassis(Set<String> chassis_keys){
-            this.allowed_chassis = chassis_keys;
-            return this;
-        }
-        
-        public Builder ForLimitedTime(Integer time){
-            x_turns = time;
-            return this;
-        }
-        
-        
-        public Restriction build() {
-            return new Restriction(this);
-        }
-    }
-    
-    private Restriction(Builder build){
-        this.allowed_chassis = build.allowed_chassis;
-        this.allowedTypes = build.allowedTypes;
-        this.allowedRoles = build.allowedRoles;
-        
-        this.length_of_effect = build.x_turns;
-     
-        this.base_bigger_than = build.base_bigger_than;
-        this.base_smaller_than = build.base_smaller_than;
-        
-        this.required_ideology = build.required_ideology;
-        this.required_facility = build.required_facility;
-    }
-      
+    private final Set<String> allowed_chassis;
+    private final Set<MovementType> allowedTypes;
+    private final Set<CombatMode> allowedRoles;
+    private final Set<String> race;
 
-    public boolean available(int lifespan, GenericUnit unit, PlayerDetails player){
+    private final Integer length_of_effect;
+
+    private final String required_ideology;
+
+    private final Integer base_bigger_than;
+    private final Integer base_smaller_than;
+    private final String required_facility;
+    
+
+    public boolean available(int lifespan, GenericUnit unit, PlayerDetails player) {
         boolean result = true;
-        
+
         result = result && length_of_effect <= lifespan;
         result = result && allowed_chassis.contains(unit.getChassis().key());
 
@@ -134,7 +68,7 @@ public class Restriction {
                 result = result && base_smaller_than < unit.getPopulation();
             }
         }
-   
+
         if (!required_facility.isEmpty()) {
             result = result && unit.getUnit_facilities().containsKey(required_facility);
         }
@@ -147,11 +81,79 @@ public class Restriction {
             result = result && allowedRoles.contains(unit.getWeapon().getCom_mode());
         }
         
-   
-        
+        if (!race.isEmpty()){
+            result = result && race.contains(player.getFaction().getRace());
+        }
+
         return result;
     }
-    
-    
+
+    public static class Builder {
+        // Required
+
+        // Optional
+        private Set<String> allowed_chassis = new LinkedHashSet<>();
+        private Set<MovementType> allowedTypes = new LinkedHashSet<>();
+        private Set<CombatMode> allowedRoles = new LinkedHashSet<>();
+        private Set races = new LinkedHashSet<>();
+
+        private Integer x_turns = null;
+
+        private Integer base_bigger_than = null;
+        private Integer base_smaller_than = null;
+
+        private String required_ideology = "";
+        private String required_facility = "";
+       
+
+        public Builder RestrictRaceTo(String race){
+            this.races.add(race);
+            return this;
+        }
+        
+        public Builder SetAllowedRoles(Set<CombatMode> commode) {
+            this.allowedRoles = commode;
+            return this;
+        }
+        
+        public Builder SetAllowedTypes(Set<MovementType> allowedTypes) {
+            this.allowedTypes = allowedTypes;
+            return this;
+        }
+
+        public Builder addAllowedType(MovementType allowedType) {
+            this.allowedTypes.add(allowedType);
+            return this;
+        }
+
+        public Builder SetAllowedChassis(Set<String> chassis_keys) {
+            this.allowed_chassis = chassis_keys;
+            return this;
+        }
+
+        public Builder ForLimitedTime(Integer time) {
+            x_turns = time;
+            return this;
+        }
+
+        public Restriction build() {
+            return new Restriction(this);
+        }
+    }
+
+    private Restriction(Builder build) {
+        this.allowed_chassis = build.allowed_chassis;
+        this.allowedTypes = build.allowedTypes;
+        this.allowedRoles = build.allowedRoles;
+
+        this.length_of_effect = build.x_turns;
+
+        this.base_bigger_than = build.base_bigger_than;
+        this.base_smaller_than = build.base_smaller_than;
+
+        this.required_ideology = build.required_ideology;
+        this.required_facility = build.required_facility;
+        this.race = build.races;
+    }
 
 }
